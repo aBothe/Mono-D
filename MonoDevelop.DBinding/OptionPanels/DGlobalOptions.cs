3bfb72c67432774a4d8a54fb63e1c143d047a6f1
michaelc37 <michaelc37@6a926a71-a401-4892-b501-a41477aafe68>
2011-09-25 18:33:37 +0000

    added a stub for D Global options (menu: Tools->Options->Other-D)

---
+++ MonoDevelop.DBinding/OptionPanels/DGlobalOptions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mono.Addins;
+using MonoDevelop.Core;
+using MonoDevelop.Ide.Projects;
+using MonoDevelop.Ide.Gui.Dialogs;
+
+namespace MonoDevelop.D.OptionPanels
+{
+	
+	public partial class DGlobalOptions : Gtk.Bin
+	{
+		private DProjectConfiguration configuration;
+		
+		public DGlobalOptions () 
+		{
+			this.Build ();
+			
+		}
+		
+		public void Load (DProjectConfiguration config)
+		{
+			configuration = config;
+			
+		}
+		
+		
+		public bool Store ()
+		{
+			if (configuration == null)
+				return false;
+			
+			return true;
+		}
+
+	}
+	
+	public class DGlobalOptionsBinding : OptionsPanel
+	{
+		private DGlobalOptions panel;
+		
+		public override Gtk.Widget CreatePanelWidget ()
+		{
+			return panel = new DGlobalOptions ();
+		}
+			
+		public override void ApplyChanges ()
+		{
+			panel.Store ();
+		}
+	}
+	
+}
