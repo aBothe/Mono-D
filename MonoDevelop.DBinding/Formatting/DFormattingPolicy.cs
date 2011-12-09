c63a93d7eea5bea8ae97a2e9e06e693bc1bab8ff
Alexander Bothe <a_bothe@gmx.net>
2011-12-09 19:43:09 +0100

    Added basic indentation support. Fixed switch/case indentations.

---
+++ MonoDevelop.DBinding/Formatting/DFormattingPolicy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoDevelop.Projects.Policies;
+
+namespace MonoDevelop.D.Formatting
+{
+	[PolicyType("D formatting")]
+	public class DFormattingPolicy:IEquatable<DFormattingPolicy>
+	{
+		static DFormattingPolicy ()
+		{
+			PolicyService.InvariantPolicies.Set<DFormattingPolicy> (new DFormattingPolicy (), "text/x-d");
+		}
+
+		public bool Equals(DFormattingPolicy other)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ MonoDevelop.DBinding/Formatting/DTextEditorIndentation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoDevelop.Ide.Gui.Content;
+using Gdk;
+using D_Parser.Formatting;
+
+namespace MonoDevelop.D.Formatting
+{
+	public class DTextEditorIndentation:TextEditorExtension
+	{
+		D_Parser.Formatting.DFormatter formatter = new D_Parser.Formatting.DFormatter();
+
+		public override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		public override bool KeyPress(Key key, char keyChar, ModifierType modifier)
+		{
+			
+			if (TextEditorProperties.IndentStyle == IndentStyle.Smart && key== Key.Return)
+			{
+				var ed = Document.Editor;
+				var cb=D_Parser.Formatting.DFormatter.CalculateIndentation(ed.Text,ed.Caret.Offset);
+
+				ed.InsertAtCaret("\n" + CalculateIndentationString(cb==null?0:cb.InnerIndentation));
+				
+			}
+			else
+				return base.KeyPress(key, keyChar, modifier);
+
+			return false;
+		}
+
+		public static string CalculateIndentationString(int indentation)
+		{
+			return TextEditorProperties.ConvertTabsToSpaces ?
+				new string(' ', indentation * TextEditorProperties.TabIndent) :
+				new string('\t', indentation);
+		}
+	}
+}
