0d31298e59d7055fe1365cef752091eac35568e2
Alexander Bothe <a_bothe@gmx.net>
2011-12-10 17:41:39 +0100

    More indentation implementations

--- MonoDevelop.DBinding/Formatting/DTextEditorIndentation.cs
+++ MonoDevelop.DBinding/Formatting/DTextEditorIndentation.cs
@@ -19,19 +19,37 @@ namespace MonoDevelop.D.Formatting
 
 		public override bool KeyPress(Key key, char keyChar, ModifierType modifier)
 		{
-			
-			if (TextEditorProperties.IndentStyle == IndentStyle.Smart && key== Key.Return)
+			if(TextEditorProperties.IndentStyle == IndentStyle.Smart)
 			{
-				var ed = Document.Editor;
-				var cb=D_Parser.Formatting.DFormatter.CalculateIndentation(ed.Text,ed.Caret.Offset);
+				if (key== Key.Return)
+				{
+					var ed = Document.Editor;
+					var cb=D_Parser.Formatting.DFormatter.CalculateIndentation(ed.Text,ed.Caret.Offset);
 
-				ed.InsertAtCaret("\n" + CalculateIndentationString(cb==null?0:cb.InnerIndentation));
+					ed.InsertAtCaret("\n" + CalculateIndentationString(cb==null?0:cb.InnerIndentation));
 				
-			}
-			else
-				return base.KeyPress(key, keyChar, modifier);
+					return false;
+				}
 
-			return false;
+				if (keyChar == '}' || keyChar == ':' || keyChar == ';')
+				{
+					var ed = Document.Editor;
+
+					ed.InsertAtCaret(keyChar.ToString());
+
+					var cb = D_Parser.Formatting.DFormatter.CalculateIndentation(ed.Text, ed.Caret.Offset);
+
+					var line=Document.Editor.GetLine(ed.Caret.Line);
+
+					ed.Replace(
+						line.Offset, 
+						ed.GetLineIndent(line).Length, 
+						CalculateIndentationString(cb == null ? 0 : cb.InnerIndentation));
+
+					return false;
+				}
+			}
+				return base.KeyPress(key, keyChar, modifier);
 		}
 
 		public static string CalculateIndentationString(int indentation)
