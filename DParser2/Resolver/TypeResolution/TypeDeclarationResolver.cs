9a79a7e1c4263d2f1064959b05688fed386e8e66
Alexander Bothe <a_bothe@gmx.net>
2012-03-03 01:15:31 +0100

    [Resolver] Fixed foreach resolution deadlock

--- DParser2/Resolver/TypeResolution/TypeDeclarationResolver.cs
+++ DParser2/Resolver/TypeResolution/TypeDeclarationResolver.cs
@@ -596,10 +596,16 @@ namespace D_Parser.Resolver.TypeResolution
 		public static ResolveResult[] GetForeachIteratorType(DVariable i, ResolverContextStack ctxt)
 		{
 			var curStmt = ctxt.ScopedStatement;
-			
-			// Walk up statement hierarchy -- note that foreach loops can be nested
-			while (curStmt != null)
-			{
+
+            bool init = true;
+            // Walk up statement hierarchy -- note that foreach loops can be nested
+            while (curStmt != null)
+            {
+                if (init)
+                    init = false;
+                else
+                    curStmt = curStmt.Parent;
+
 				if (curStmt is ForeachStatement)
 				{
 					var fe = (ForeachStatement)curStmt;
