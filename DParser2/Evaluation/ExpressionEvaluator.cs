c2e4f2661b66b1770b5b76a1927141beb64d6596
Alexander Bothe <a_bothe@gmx.net>
2012-03-14 21:54:28 +0100

    [Expression Evaluator] Began with the expression eval stuff -- added few class stubs

---
+++ DParser2/Evaluation/ExpressionEvaluator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using D_Parser.Dom.Expressions;
+using D_Parser.Resolver;
+
+namespace D_Parser.Evaluation
+{
+	public class ExpressionEvaluator
+	{
+		ResolverContextStack ctxt;
+
+		private ExpressionEvaluator() { }
+
+		public static object Evaluate(IExpression expression, ResolverContextStack ctxt)
+		{
+			return new ExpressionEvaluator { ctxt = ctxt }.Evaluate(expression);
+		}
+
+		public object Evaluate(IExpression x)
+		{
+			return null;
+		}
+
+		#region Helpers
+		public static bool ToBool(object value)
+		{
+			bool b = false;
+
+			try
+			{
+				b = Convert.ToBoolean(value);
+			}
+			catch { }
+
+			return b;
+		}
+
+		public static double ToDouble(object value)
+		{
+			double d = 0;
+
+			try
+			{
+				d = Convert.ToDouble(value);
+			}
+			catch { }
+
+			return d;
+		}
+
+		public static long ToLong(object value)
+		{
+			long d = 0;
+
+			try
+			{
+				d = Convert.ToInt64(value);
+			}
+			catch { }
+
+			return d;
+		}
+		#endregion
+	}
+}
---
+++ DParser2/Evaluation/PrimitiveValue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D_Parser.Evaluation
+{
+	public struct PrimitiveValue
+	{
+		public readonly PrimitiveType Type;
+		public readonly object Value;
+
+		public PrimitiveValue(PrimitiveType ValueType, object Value)
+		{
+			this.Type = ValueType;
+			this.Value = Value;
+		}
+
+		/// <summary>
+		/// Returns true if the represented value is either null (ref type), 0 (int/float), false (bool) or empty (string)
+		/// </summary>
+		public bool IsNullFalseOrEmpty
+		{
+			get {
+				if (Value == null)
+					return true;
+
+				try
+				{
+					switch (Type)
+					{
+						case PrimitiveType.Bool:
+							return !Convert.ToBoolean(Value);
+						case PrimitiveType.Char:
+							var c = Convert.ToChar(Value);
+
+							return c == '\0';
+					}
+				}
+				catch {}
+				return false;
+			}
+		}
+	}
+
+	public enum PrimitiveType
+	{
+		Bool,
+		Char,
+		Int,
+		Float,
+		String,
+		Reference
+	}
+}
