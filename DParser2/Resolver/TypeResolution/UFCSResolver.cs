a9586731913a29cb7a65fd1c100b4d55242fe90b
Alexander Bothe <a_bothe@gmx.net>
2012-03-31 12:38:43 +0200

    [Resolver] Added ufcs resolution stub

---
+++ DParser2/Resolver/TypeResolution/UFCSResolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D_Parser.Resolver.TypeResolution
+{
+	/// <summary>
+	/// UFCS: User function call syntax;
+	/// A base expression will be used as a method'd first call parameter 
+	/// so it looks like the first expression had a respective sub-method.
+	/// Example:
+	/// assert("fdas".reverse() == "asdf"); -- reverse() will be called with "fdas" as the first argument.
+	/// 
+	/// </summary>
+	public class UFCSResolver
+	{
+	}
+}
