0880e207124738a5c4e3a43a878f1e727bad3e68
krzat <qwertyszot@gmail.com>
2012-10-21 22:22:06 +0200

    Added CodeAction (Alt + Enter) for fast importing

---
+++ MonoDevelop.DBinding/Refactoring/CodeActions/DCodeActionSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MonoDevelop.CodeActions;
+namespace MonoDevelop.D.Refactoring.CodeActions
+{
+    public class DCodeActionSource : ICodeActionProviderSource
+    {
+        List<CodeActionProvider> providers;
+        public DCodeActionSource()
+        {
+            providers = new List<CodeActionProvider>();
+            providers.Add(new ImportSymbol());
+        }
+        public IEnumerable<CodeActionProvider> GetProviders()
+        {
+            return providers;
+        }
+    }
+}
