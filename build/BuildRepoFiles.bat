5a73eba9728750183b828370843a47cf02a76344
aBothe <info@alexanderbothe.com>
2012-11-13 09:02:51 +0100

    .

--- build/BuildRepoFiles.bat
+++ build/BuildRepoFiles.bat
@@ -1,4 +1,6 @@
 @echo off
 
-mdtool setup pack ..\MonoDevelop.DBinding\bin\Debug\MonoDevelop.D.dll
-mdtool setup rep-build .
+set P=..\..\tutorial.lib\monodevelop\main\build\bin\mdtool.exe
+
+%P% setup pack ..\MonoDevelop.DBinding\bin\Debug\MonoDevelop.D.dll
+%P% setup rep-build .
