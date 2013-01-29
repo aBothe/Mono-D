a1e105767a1943332983184800c970cc4a5a84b8
aBothe <info@alexanderbothe.com>
2013-01-29 22:44:49 +0100

    Made syntax mode static/singleton - which might improves doc opening performance.

--- MonoDevelop.DBinding/Highlighting/DSyntaxMode.cs
+++ MonoDevelop.DBinding/Highlighting/DSyntaxMode.cs
@@ -9,26 +9,30 @@ namespace MonoDevelop.D.Highlighting
 {
 	public class DSyntaxMode : SyntaxMode
 	{
+		static SyntaxMode baseMode;
+
 		public DSyntaxMode()
 		{
 			var matches = new List<Mono.TextEditor.Highlighting.Match>();
 
-			var provider = new ResourceXmlProvider(
-				typeof(DSyntaxMode).Assembly, 
-				typeof(DSyntaxMode).Assembly.GetManifestResourceNames().First(s => s.Contains("DSyntaxHighlightingMode")));
-			using (XmlReader reader = provider.Open())
+			if (baseMode == null)
 			{
-				SyntaxMode baseMode = SyntaxMode.Read(reader);
-				this.rules = new List<Rule>(baseMode.Rules);
-				this.keywords = new List<Keywords>(baseMode.Keywords);
-				this.spans = new List<Span>(baseMode.Spans.Where(span => span.Begin.Pattern != "#")).ToArray();
-				matches.AddRange(baseMode.Matches);
-				this.prevMarker = baseMode.PrevMarker;
-				this.SemanticRules = new List<SemanticRule>(baseMode.SemanticRules);
-				this.keywordTable = baseMode.keywordTable;
-				this.keywordTableIgnoreCase = baseMode.keywordTableIgnoreCase;
-				this.properties = baseMode.Properties;
+				var provider = new ResourceXmlProvider(
+					typeof(DSyntaxMode).Assembly,
+					typeof(DSyntaxMode).Assembly.GetManifestResourceNames().First(s => s.Contains("DSyntaxHighlightingMode")));
+				using (XmlReader reader = provider.Open())
+					baseMode = SyntaxMode.Read(reader);
 			}
+
+			this.rules = new List<Rule>(baseMode.Rules);
+			this.keywords = new List<Keywords>(baseMode.Keywords);
+			this.spans = new List<Span>(baseMode.Spans.Where(span => span.Begin.Pattern != "#")).ToArray();
+			matches.AddRange(baseMode.Matches);
+			this.prevMarker = baseMode.PrevMarker;
+			this.SemanticRules = new List<SemanticRule>(baseMode.SemanticRules);
+			this.keywordTable = baseMode.keywordTable;
+			this.keywordTableIgnoreCase = baseMode.keywordTableIgnoreCase;
+			this.properties = baseMode.Properties;
 
 			// D Number literals
 			matches.Add(workaroundMatchCtor("constant.digit", @"(?<!\w)(0((x|X)[0-9a-fA-F_]+|(b|B)[0-1_]+)|([0-9]+[_0-9]*)[L|U|u|f|i]*)"));
@@ -47,7 +51,7 @@ namespace MonoDevelop.D.Highlighting
 			this.matches = matches.ToArray();
 		}
 
-		Match workaroundMatchCtor(string color, string regex)
+		static Match workaroundMatchCtor(string color, string regex)
 		{
 			var st = new StringReader("<Match color = \""+color+"\"><![CDATA["+regex+"]]></Match>");
 
