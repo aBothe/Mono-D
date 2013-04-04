dee95c1f267decbebf784da32050a036f36bdd98
aBothe <info@alexanderbothe.com>
2013-04-04 11:08:20 +0200

    began with externalizing project dep panel from the D project options to a stand-alone option panel

---
+++ MonoDevelop.DBinding/OptionPanels/ProjectDependencyPanelBinding.cs
@@ -0,0 +1,28 @@
+using System;
+using MonoDevelop.Ide.Gui.Dialogs;
+
+namespace MonoDevelop.D
+{
+	public class ProjectDependencyPanelBinding : MultiConfigItemOptionsPanel
+	{
+		public ProjectDependencyPanelBinding ()
+		{
+		}
+		
+		public override Widget CreatePanelWidget ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public override void ApplyChanges ()
+		{
+			throw new NotImplementedException ();
+		}
+		
+		public override void LoadConfigData ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
