21d3183604254ab9d85f3b078563a14be66a3ab0
aBothe <info@alexanderbothe.com>
2013-04-06 16:17:34 +0200

    began with implementing package.json file format for extended DUB support.

---
+++ MonoDevelop.DBinding/Project/DubProject.cs
@@ -0,0 +1,52 @@
+﻿using MonoDevelop.Projects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace MonoDevelop.D.Dub
+{
+	public class DubProject : WorkspaceItem
+	{
+		string name;
+		string description;
+		string homepage;
+		string copyright;
+		List<string> authors = new List<string>();
+		Dictionary<string, DubProjectDependency> dependencies = new Dictionary<string, DubProjectDependency>();
+		/*
+		{
+			set {
+				var deps = new Dictionary<string, DubProjectDependency>();
+
+				if (value != null)
+					foreach (var kv in value)
+						deps[kv.Key] = new DubProjectDependency(kv.Key,kv.Value);
+
+				Dependencies = deps;
+			}
+		}*/
+
+		public override string Name { get { return name; } set { name = value; } }
+		public string Description { get { return description; } set { description = value; } }
+		public string Homepage { get { return homepage; } set { homepage = value; } }
+		public string Copyright { get { return copyright; } set { copyright = value; } }
+		public List<string> Authors { get { return authors; } }
+		public Dictionary<string, DubProjectDependency> Dependencies
+		{
+			get { return dependencies; }
+		}
+	}
+
+	public class DubProjectDependency
+	{
+		string name;
+		string version;
+		string path;
+
+		public string Name { get { return name; } set { name = value; } }
+		public string Version { get { return version; } set { version = value; } }
+		public string Path { get { return path; } set { path = value; } }
+	}
+}
---
+++ MonoDevelop.DBinding/Project/PackageJsonParser.cs
@@ -0,0 +1,115 @@
+﻿using MonoDevelop.Core;
+using MonoDevelop.Projects.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MonoDevelop.D.Dub
+{
+	public class PackageJsonParser : IFileFormat
+	{
+		public bool CanReadFile(FilePath file, Type expectedObjectType)
+		{
+			return file.FileName == "package.json";
+		}
+
+		public bool CanWriteFile(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ConvertToFormat(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<string> GetCompatibilityWarnings(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Core.FilePath> GetItemFiles(object obj)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Core.FilePath GetValidFormatName(object obj, Core.FilePath fileName)
+		{
+			throw new NotImplementedException();
+		}
+
+		class DepConverter : Newtonsoft.Json.Converters.CustomCreationConverter<DubProjectDependency>
+		{
+			bool isReading = false;
+
+			public override bool CanConvert(Type objectType)
+			{
+				return base.CanConvert(objectType) && !isReading;
+			}
+
+			public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+			{
+				string name;
+				var s = reader.Path.Split('.');
+				
+				name = s[s.Length-1];
+
+				DubProjectDependency dpd;
+				if (reader.TokenType == JsonToken.String)
+					dpd = new DubProjectDependency { Version = reader.Value as string };
+				else
+				{
+					isReading = true;
+					try
+					{
+						dpd = serializer.Deserialize<DubProjectDependency>(reader);
+					}
+					finally { isReading = false; }
+				}
+
+				dpd.Name = name;
+				return dpd;
+			}
+
+			public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+			{
+				throw new NotImplementedException();
+			}
+
+			public override DubProjectDependency Create(Type objectType)
+			{
+				return new DubProjectDependency();
+			}
+		}
+
+		public object ReadFile(FilePath file, Type expectedType, IProgressMonitor monitor)
+		{
+			var settings = new JsonSerializerSettings();
+			var json = File.ReadAllText(file);
+			
+			settings.Converters.Add(new DepConverter());
+			
+			var dp = JsonConvert.DeserializeObject<DubProject>(json, settings);
+			
+			return dp;
+		}
+
+		public bool SupportsFramework(Core.Assemblies.TargetFramework framework)
+		{
+			return false;
+		}
+
+		public bool SupportsMixedFormats
+		{
+			get { return false; }
+		}
+
+		public void WriteFile(Core.FilePath file, object obj, Core.IProgressMonitor monitor)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
