29afaaa69b180202fd956299b3b036c787daea06
aBothe <info@alexanderbothe.com>
2014-02-02 21:42:18 +0100

    .

--- make symlink.bat
+++ make symlink.bat
@@ -3,9 +3,7 @@
 rem Creates a symlink between the Mono-D output directory and the addins dictionary of MonoDevelop
 rem to save copying the lib files over and over again
 
-rem This batch file must be ran as admin!
-
 rem TODO: Fit to target path
-mklink /D /J "C:\Program Files (x86)\Xamarin Studio\AddIns\mono-d" MonoDevelop.DBinding\bin\Debug
+mklink /D /J "%APPDATA%\..\Local\XamarinStudio-4.0\LocalInstall\Addins\mono-d" MonoDevelop.DBinding\bin\Debug
 
 pause
