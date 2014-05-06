8e221d4042ecb8e5e2fdbefac1c165c8fc7a92a3
aBothe <info@alexanderbothe.com>
2014-05-06 14:30:35 +0200

    [Debugging] Began with insourcing locals&parameter backtracing for debugger addins

---
+++ MonoDevelop.DBinding/Debugging/DLocalExamBacktrace.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Debugging.Client;
+using Mono.Debugging.Backend;
+
+namespace MonoDevelop.D.Debugging
+{
+	public class DLocalExamBacktrace : IObjectValueSource
+	{
+		public readonly IDBacktraceHelpers BacktraceHelper;
+
+		public DLocalExamBacktrace(IDBacktraceHelpers helper)
+		{
+			this.BacktraceHelper = helper;
+		}
+
+
+
+		public ObjectValue CreateObjectValue(IDBacktraceSymbol s)
+		{
+			return ObjectValue.CreateError(this, new ObjectPath(), s.TypeName, s.Value, ObjectValueFlags.Error);
+		}
+
+		public ObjectValue[] GetChildren(ObjectPath path, int index, int count, EvaluationOptions options)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object GetRawValue(ObjectPath path, EvaluationOptions options)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ObjectValue GetValue(ObjectPath path, EvaluationOptions options)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetRawValue(ObjectPath path, object value, EvaluationOptions options)
+		{
+			throw new NotImplementedException();
+		}
+
+		public EvaluationResult SetValue(ObjectPath path, string value, EvaluationOptions options)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ MonoDevelop.DBinding/Debugging/IDBacktraceHelpers.cs
@@ -0,0 +1,42 @@
+﻿using D_Parser.Resolver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoDevelop.D.Debugging
+{
+	public interface IDBacktraceSymbol
+	{
+		ulong Offset { get; }
+		string Name { get; }
+		string TypeName { get; }
+		string Value { get; }
+		string FileName { get; }
+
+		bool HasParent { get; }
+		IDBacktraceSymbol Parent { get; }
+
+		int ChildCount { get; }
+		IEnumerable<IDBacktraceSymbol> Children { get; }
+	}
+
+	public interface IDBacktraceHelpers
+	{
+		void SelectStackFrame(int frameIndex);
+
+		IEnumerable<IDBacktraceSymbol> Parameters { get; }
+		IEnumerable<IDBacktraceSymbol> Locals { get; }
+
+		int PointerByteSize { get; }
+
+		byte[] ReadBytes(ulong offset, ulong size);
+		byte ReadByte(ulong offset);
+		short ReadInt16(ulong offset);
+		int ReadInt32(ulong offset);
+		long ReadInt64(ulong offset);
+
+		ResolutionContext LocalsResolutionHelperContext { get; }
+		void UpdateHelperContextToCurrentStackFrame();
+	}
+}
