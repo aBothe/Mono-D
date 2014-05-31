963793e0459cf67ad1585fd4208942244a477818
aBothe <info@alexanderbothe.com>
2014-05-31 16:02:58 +0200

    [Editing/Gui] #492 Have a user code region dropdown in the AST path bar.

--- MonoDevelop.DBinding/Gui/EditorPathBarExtension.cs
+++ MonoDevelop.DBinding/Gui/EditorPathBarExtension.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using D_Parser.Dom;
-using D_Parser.Dom.Statements;
+﻿using D_Parser.Dom;
 using D_Parser.Resolver.TypeResolution;
 using Gtk;
 using MonoDevelop.Components;
@@ -9,7 +6,12 @@ using MonoDevelop.Core;
 using MonoDevelop.D.Completion;
 using MonoDevelop.D.Parser;
 using MonoDevelop.Ide;
+using MonoDevelop.Ide.Gui;
 using MonoDevelop.Ide.Gui.Content;
+using MonoDevelop.Ide.TypeSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MonoDevelop.D.Gui
 {
@@ -51,23 +53,36 @@ namespace MonoDevelop.D.Gui
 
 		public Widget CreatePathWidget(int index)
 		{
-			PathEntry[] path = CurrentPath;
-			if (null == path || 0 > index || path.Length <= index)
-			{
+			var path = CurrentPath;
+			if (null == path || index < 0 || path.Length <= index)
 				return null;
-			}
 
-			object tag = path[index].Tag;
-			DropDownBoxListWindow.IListDataProvider provider = null;
-			if (!((tag is IBlockNode) || (tag is DEnumValue) || (tag is NoSelectionCustomNode)))
-			{
-				return null;
-			}
-			provider = new EditorPathbarProvider(Document, tag);
-
-			var window = new DropDownBoxListWindow(provider);
+			var tag = path[index].Tag;
+			var window = new DropDownBoxListWindow(tag == null ? (MonoDevelop.Components.DropDownBoxListWindow.IListDataProvider)new CompilationUnitDataProvider(Document) : new EditorPathbarProvider(Document, tag));
+			
 			window.SelectItem(tag);
+
 			return window;
+		}
+
+
+		static PathEntry GetRegionEntry(ParsedDocument unit, Mono.TextEditor.DocumentLocation loc)
+		{
+			PathEntry entry;
+			if (!unit.UserRegions.Any())
+				return null;
+			var reg = unit.UserRegions.LastOrDefault(r => r.Region.IsInside(loc));
+			if (reg == null)
+			{
+				entry = new PathEntry(GettextCatalog.GetString("No region"));
+			}
+			else
+			{
+				entry = new PathEntry(CompilationUnitDataProvider.Pixbuf,
+									   GLib.Markup.EscapeText(reg.Name));
+			}
+			entry.Position = EntryPosition.Right;
+			return entry;
 		}
 
 		protected virtual void OnPathChanged(DocumentPathChangedEventArgs args)
@@ -109,11 +124,10 @@ namespace MonoDevelop.D.Gui
 
 			List<PathEntry> result = new List<PathEntry>();
 			INode node = currentblock;
+			PathEntry entry;
 
 			while ((node != null) && ((node is IBlockNode) || (node is DEnumValue)))
 			{
-				PathEntry entry;
-
 				var icon = DIcons.GetNodeIcon(node as DNode);
 
 				entry = new PathEntry(icon.IsNull?null: ImageService.GetIcon(icon.Name, IconSize.Menu), node.Name + DParameterDataProvider.GetNodeParamString(node));
@@ -131,9 +145,82 @@ namespace MonoDevelop.D.Gui
 				result.Add(noSelection);
 			}
 
+			entry = GetRegionEntry(Document.ParsedDocument, Document.Editor.Caret.Location);
+			if (entry != null)
+				result.Add(entry);
+
 			var prev = CurrentPath;
 			CurrentPath = result.ToArray();
 			OnPathChanged(new DocumentPathChangedEventArgs(prev));
 		}
+
+
+		#region Region dropdown
+
+		class CompilationUnitDataProvider : DropDownBoxListWindow.IListDataProvider
+		{
+			Document Document
+			{
+				get;
+				set;
+			}
+
+			public CompilationUnitDataProvider(Document document)
+			{
+				this.Document = document;
+			}
+
+			#region IListDataProvider implementation
+
+			public void Reset()
+			{
+			}
+
+			public string GetMarkup(int n)
+			{
+				return GLib.Markup.EscapeText(Document.ParsedDocument.UserRegions.ElementAt(n).Name);
+			}
+
+			internal static Xwt.Drawing.Image Pixbuf
+			{
+				get
+				{
+					return ImageService.GetIcon(Gtk.Stock.Add, Gtk.IconSize.Menu);
+				}
+			}
+
+			public Xwt.Drawing.Image GetIcon(int n)
+			{
+				return Pixbuf;
+			}
+
+			public object GetTag(int n)
+			{
+				return Document.ParsedDocument.UserRegions.ElementAt(n);
+			}
+
+			public void ActivateItem(int n)
+			{
+				var reg = Document.ParsedDocument.UserRegions.ElementAt(n);
+				var extEditor = Document.GetContent<MonoDevelop.Ide.Gui.Content.IExtensibleTextEditor>();
+				if (extEditor != null)
+					extEditor.SetCaretTo(Math.Max(1, reg.Region.BeginLine), reg.Region.BeginColumn);
+			}
+
+			public int IconCount
+			{
+				get
+				{
+					if (Document.ParsedDocument == null)
+						return 0;
+					return Document.ParsedDocument.UserRegions.Count();
+				}
+			}
+
+			#endregion
+
+		}
+
+		#endregion
 	}
 }
