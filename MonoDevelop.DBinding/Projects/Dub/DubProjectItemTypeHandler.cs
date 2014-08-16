33a79786c7c8baf682e1a2c7dd7dc35a00240b40
aBothe <info@alexanderbothe.com>
2014-08-16 23:46:17 +0200

    [Projects] Probably fixed #432, #525 adding a dub project to a non-dub or not even non-D solution

---
+++ MonoDevelop.DBinding/Projects/Dub/DubProjectItemTypeHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Mono.Addins;
+using MonoDevelop.Projects.Formats.MSBuild;
+using MonoDevelop.Core;
+using MonoDevelop.Projects.Extensions;
+using MonoDevelop.Projects;
+
+namespace MonoDevelop.D.Projects.Dub
+{
+	class DubProjectItemTypeHandler : ItemTypeNode
+	{
+		public Type ItemType
+		{
+			get { return typeof(DubProject); }
+		}
+
+		public override bool CanHandleItem(SolutionEntityItem item)
+		{
+			return ItemType != null && ItemType.IsAssignableFrom(item.GetType());
+		}
+
+		public override bool CanHandleFile(string fileName, string typeGuid)
+		{
+			return string.Compare(typeGuid, this.Guid, true) == 0 || 
+				(fileName = fileName.ToLower()).EndsWith(PackageJsonParser.DubJsonFile) || 
+				fileName.EndsWith(PackageJsonParser.PackageJsonFile);
+		}
+
+		public override SolutionEntityItem LoadSolutionItem(IProgressMonitor monitor, string fileName, MSBuildFileFormat expectedFormat, string itemGuid)
+		{
+			return DubProject.LoadProject(fileName, monitor);
+		}
+	}
+}
