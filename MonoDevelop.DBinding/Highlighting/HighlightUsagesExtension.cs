ad3b04d0fcacd69c22861f344a56de28989d0f59
Alexander Bothe <info@alexanderbothe.com>
2015-02-19 22:56:47 +0100

    Adapt to new ResolutionContext.CancellationToken API

--- MonoDevelop.DBinding/Highlighting/HighlightUsagesExtension.cs
+++ MonoDevelop.DBinding/Highlighting/HighlightUsagesExtension.cs
@@ -58,8 +58,7 @@ namespace MonoDevelop.D.Highlighting
 		protected override IEnumerable<Ide.FindInFiles.MemberReference> GetReferences(Tuple<ResolutionContext, DSymbol> tup, System.Threading.CancellationToken token)
 		{
 			var ctxt = tup.Item1;
-			ctxt.CancelOperation = false;
-			token.Register(()=>ctxt.CancelOperation = true);
+			ctxt.CancellationToken = token;
 			var mr = tup.Item2;
 
 			var referencedNode = mr.Definition;
